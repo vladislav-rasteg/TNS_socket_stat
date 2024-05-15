1a881152a64922108d716e486bfdab24323d244b
vladrast <rastegaev.design@gmail.com>
2024-05-15 20:10:18 +0500

    feat: ws

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 require('dotenv').config()
+const cors = require('cors')
 const express = require('express')
+const { Server } = require("socket.io");
+const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors());
 app.use(express.json())
+
 const server = require('http').createServer(app);
 
-app.get('/', (req, res) => res.send('Hello world!'));
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+})
 
+let latestMessage = null;
+
+function handleMessageFromOtherServer(message) {
+    latestMessage = message;
+}
+
+setInterval(() => {
+    if (latestMessage !== null) {
+        io.emit("recieve_message", latestMessage);
+        latestMessage = null;
+    }
+}, 1000);
+
+io.on('connection', (socket) => {
+    console.log('A new client connected')
+
+    socket.on("message", (message) => {
+        handleMessageFromOtherServer(message)
+    })
+})
+
+app.get('/', (req, res) => res.send('Hello world!'));
 
 const start = async () => { 
     try{
