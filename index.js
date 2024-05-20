53b8cfea7b5e435367406ba847a628dcf3d390a6
rastegaev <i@ivlad.ru>
2024-05-20 11:54:04 +0500

    feat

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const { default: axios } = require('axios');
 const cors = require('cors')
 const express = require('express')
 const { Server } = require("socket.io");
@@ -15,29 +16,19 @@ const server = require('http').createServer(app);
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:5173",
-        methods: ["GET", "POST"]
+        methods: ["GET", "POST"] 
     }
 })
 
-let latestMessage = null;
-
-function handleMessageFromOtherServer(message) {
-    latestMessage = message;
-}
-
 setInterval(() => {
-    if (latestMessage !== null) {
-        io.emit("recieve_message", latestMessage);
-        latestMessage = null;
-    }
+    axios.get(process.env.API_URL+"/stat/latest")
+        .then(data => {
+            io.emit("recieve_message", data.data);
+        })
 }, 1000);
 
 io.on('connection', (socket) => {
     console.log('A new client connected')
-
-    socket.on("message", (message) => {
-        handleMessageFromOtherServer(message)
-    })
 })
 
 app.get('/', (req, res) => res.send('Hello world!'));
