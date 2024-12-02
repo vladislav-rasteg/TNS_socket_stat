2bd283852ed24627406f9a5c66b9f5547d38ec11
Vladislav Rastegaev <99723680+vladislav-rasteg@users.noreply.github.com>
2024-12-02 14:58:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ const server = require('http').createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"] 
+        origin: "*"
     },
     transports: ["websocket", "polling"]
 })
