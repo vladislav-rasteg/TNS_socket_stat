38a64bc5a2ca21a2fda730aa016b55915b6c548b
Vladislav Rastegaev <99723680+vladislav-rasteg@users.noreply.github.com>
2024-12-02 14:46:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const io = new Server(server, {
         origin: "*",
         methods: ["GET", "POST"] 
     },
-    transports: ["websocket"]
+    transports: ["websocket", "poling"]
 })
 
 setInterval(() => {
