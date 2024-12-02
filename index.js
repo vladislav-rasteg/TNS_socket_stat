de2be0fd306dc92bda9a9fd26d0f6553251953bc
Vladislav Rastegaev <99723680+vladislav-rasteg@users.noreply.github.com>
2024-12-02 14:38:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = require('http').createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://premarket.trustname.org",
+        origin: "*",
         methods: ["GET", "POST"] 
     }
 })
